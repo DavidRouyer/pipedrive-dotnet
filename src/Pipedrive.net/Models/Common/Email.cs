af56811f44e90ebc9ac60f57e40831a7f51282f6
David Rouyer <rouyer.david@gmail.com>
2019-04-18 18:04:16 +0200

    feat: add labels on email and phone

--- src/Pipedrive.net/Models/Common/Email.cs
+++ src/Pipedrive.net/Models/Common/Email.cs
@@ -4,6 +4,9 @@ namespace Pipedrive
 {
     public class Email
     {
+        [JsonProperty("label")]
+        public string Label { get; set; }
+
         [JsonProperty("value")]
         public string Value { get; set; }
 
