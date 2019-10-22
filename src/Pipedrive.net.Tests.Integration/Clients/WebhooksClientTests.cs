ab6317754cbdc0650cd46d7069d23bc79da91038
David Rouyer <rouyer.david@gmail.com>
2019-10-22 17:12:12 +0200

    feat: feat: webhook response deserializer for persons

--- src/Pipedrive.net.Tests.Integration/Clients/WebhooksClientTests.cs
+++ src/Pipedrive.net.Tests.Integration/Clients/WebhooksClientTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -68,6 +69,24 @@ namespace Pipedrive.Tests.Integration.Clients
 
                     var webhookClient = new WebhooksClient();
                     var result = webhookClient.ParseWebhookOrganizationResponse(fixture);
+                }
+            }
+        }
+
+        public class TheParseWebhookPersonResponseMethod
+        {
+            [Fact]
+            public async Task DeserializeUpdateInformations()
+            {
+                var stream = Helper.LoadFixture("webhook_person_update.json");
+
+                using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    var fixture = await reader.ReadToEndAsync();
+
+                    var webhookClient = new WebhooksClient();
+                    var result = webhookClient.ParseWebhookPersonResponse(fixture);
+                    Console.WriteLine(result.Current.AddTime);
                 }
             }
         }
---
+++ src/Pipedrive.net/Models/Response/WebhookPerson.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Pipedrive.Internal;
+
+namespace Pipedrive
+{
+    [JsonConverter(typeof(CustomFieldConverter))]
+    public class WebhookPerson : IEntityWithCustomFields
+    {
+        public long Id { get; set; }
+
+        [JsonProperty("company_id")]
+        public long CompanyId { get; set; }
+
+        [JsonProperty("owner_id")]
+        public long OwnerId { get; set; }
+
+        [JsonProperty("org_id")]
+        public long OrgId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("open_deals_count")]
+        public int OpenDealsCount { get; set; }
+
+        [JsonProperty("related_open_deals_count")]
+        public int RelatedOpenDealsCount { get; set; }
+
+        [JsonProperty("closed_deals_count")]
+        public int ClosedDealsCount { get; set; }
+
+        [JsonProperty("related_closed_deals_count")]
+        public int RelatedClosedDealsCount { get; set; }
+
+        [JsonProperty("participant_open_deals_count")]
+        public int ParticipantOpenDealsCount { get; set; }
+
+        [JsonProperty("participant_closed_deals_count")]
+        public int ParticipantClosedDealsCount { get; set; }
+
+        [JsonProperty("email_messages_count")]
+        public int EmailMessagesCount { get; set; }
+
+        [JsonProperty("activities_count")]
+        public int ActivitiesCount { get; set; }
+
+        [JsonProperty("done_activities_count")]
+        public int DoneActivitiesCount { get; set; }
+
+        [JsonProperty("undone_activities_count")]
+        public int UndoneActivitiesCount { get; set; }
+
+        [JsonProperty("reference_activities_count")]
+        public int ReferenceActivitiesCount { get; set; }
+
+        [JsonProperty("files_count")]
+        public int FilesCount { get; set; }
+
+        [JsonProperty("notes_count")]
+        public int NotesCount { get; set; }
+
+        [JsonProperty("followers_count")]
+        public int FollowersCount { get; set; }
+
+        [JsonProperty("won_deals_count")]
+        public int WonDealsCount { get; set; }
+
+        [JsonProperty("related_won_deals_count")]
+        public int RelatedWonDealsCount { get; set; }
+
+        [JsonProperty("lost_deals_count")]
+        public int LostDealsCount { get; set; }
+
+        [JsonProperty("related_lost_deals_count")]
+        public int RelatedLostDealsCount { get; set; }
+
+        [JsonProperty("active_flag")]
+        public bool ActiveFlag { get; set; }
+
+        [JsonProperty("phone")]
+        public List<Phone> Phone { get; set; }
+
+        [JsonProperty("email")]
+        public List<Email> Email { get; set; }
+
+        [JsonProperty("first_char")]
+        public char FirstChar { get; set; }
+
+        [JsonProperty("update_time")]
+        public DateTime? UpdateTime { get; set; }
+
+        [JsonProperty("add_time")]
+        public DateTime AddTime { get; set; }
+
+        [JsonProperty("visible_to")]
+        public Visibility VisibleTo { get; set; }
+
+        [JsonProperty("picture_id")]
+        public Picture PictureId { get; set; }
+
+        [JsonProperty("next_activity_date")]
+        public string NextActivityDate { get; set; }
+
+        [JsonProperty("next_activity_time")]
+        public string NextActivityTime { get; set; }
+
+        [JsonProperty("next_activity_id")]
+        public long? NextActivityId { get; set; }
+
+        [JsonProperty("last_activity_id")]
+        public long? LastActivityId { get; set; }
+
+        [JsonProperty("last_activity_date")]
+        public string LastActivityDate { get; set; }
+
+        [JsonProperty("timeline_last_activity_time")]
+        public string TimelineLastActivityTime { get; set; }
+
+        [JsonProperty("timeline_last_activity_time_by_owner")]
+        public string TimelineLastActivityTimeByOwner { get; set; }
+
+        [JsonProperty("last_incoming_mail_time")]
+        public DateTime? LastIncomingMailTime { get; set; }
+
+        [JsonProperty("last_outgoing_mail_time")]
+        public DateTime? LastOutgoingMailTime { get; set; }
+
+        [JsonProperty("org_name")]
+        public string OrgName { get; set; }
+
+        [JsonProperty("owner_name")]
+        public string OwnerName { get; set; }
+
+        [JsonProperty("cc_email")]
+        public string CcEmail { get; set; }
+
+        [JsonIgnore]
+        public IDictionary<string, ICustomField> CustomFields { get; set; }
+
+        public PersonUpdate ToUpdate()
+        {
+            return new PersonUpdate
+            {
+                Name = Name,
+                Email = Email,
+                Phone = Phone,
+                OrgId = OrgId,
+                OwnerId = OwnerId,
+                VisibleTo = VisibleTo,
+                CustomFields = CustomFields
+            };
+        }
+    }
+}
