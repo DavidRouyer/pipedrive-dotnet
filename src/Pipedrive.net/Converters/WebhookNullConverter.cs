8928734e31d41b3e5e7604a3e2db89d62f275812
David Rouyer <rouyer.david@gmail.com>
2020-01-04 14:03:03 +0100

    chore: cleanup

--- src/Pipedrive.net/Converters/WebhookNullConverter.cs
+++ src/Pipedrive.net/Converters/WebhookNullConverter.cs
@@ -1,19 +1,16 @@
-﻿
-    using System;
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Linq;
-    
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace Pipedrive.Converters
 {
     public class WebhookNullConverter : JsonConverter
     {
-        /// <inheritdoc/>
         public override bool CanConvert(Type objectType)
         {
             return true;
         }
 
-        /// <inheritdoc/>
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var token = JToken.Load(reader);
@@ -25,7 +22,6 @@ namespace Pipedrive.Converters
             return token.ToObject(objectType);
         }
 
-        /// <inheritdoc/>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
             throw new System.NotImplementedException();
