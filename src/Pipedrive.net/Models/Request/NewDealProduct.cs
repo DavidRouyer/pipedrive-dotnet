55ebfbe65ef85cb2fc45a7636072d296e1350ea1
David Rouyer <rouyer.david@gmail.com>
2020-03-04 00:32:50 +0100

    feat: product endpoints (#42)

---
+++ src/Pipedrive.net/Models/Request/NewDealProduct.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+
+namespace Pipedrive
+{
+    public class NewDealProduct
+    {
+        [JsonProperty("product_id")]
+        public long ProductId { get; set; }
+
+        [JsonProperty("item_price")]
+        public decimal ItemPrice { get; set; }
+
+        [JsonProperty("quantity")]
+        public int Quantity { get; set; }
+
+        [JsonProperty("discount_percentage")]
+        public decimal DiscountPercentage { get; set; }
+
+        [JsonProperty("duration")]
+        public long Duration { get; set; } = 1;
+
+        [JsonProperty("product_variation_id")]
+        public long? ProductVariationId { get; set; }
+
+        [JsonProperty("comments")]
+        public string Comments { get; set; }
+
+        [JsonProperty("enabled_flag")]
+        public bool EnabledFlag { get; set; } = true;
+
+        public NewDealProduct(long productId, decimal itemPrice, int quantity)
+        {
+            ProductId = productId;
+            ItemPrice = itemPrice;
+            Quantity = quantity;
+        }
+    }
+}
---
+++ src/Pipedrive.net/Models/Request/NewProductPrice.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace Pipedrive.Models.Request
+{
+    public class NewProductPrice
+    {
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("cost")]
+        public decimal? Cost { get; set; }
+
+        [JsonProperty("overhead_cost")]
+        public decimal? OverheadCost { get; set; }
+    }
+}
