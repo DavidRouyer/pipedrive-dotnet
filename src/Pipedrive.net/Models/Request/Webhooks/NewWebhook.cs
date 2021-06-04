7a7eff31a9aa610a51e27373af3b9b76519324e7
David Rouyer <rouyer.david@gmail.com>
2021-06-04 17:47:21 +0200

    chore: reorg

--- src/Pipedrive.net/Models/Request/Webhooks/NewWebhook.cs
+++ src/Pipedrive.net/Models/Request/Webhooks/NewWebhook.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Serialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Pipedrive.Models.Common.Webhooks;
 
