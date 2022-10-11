8ff57339668a6dc9b29e8ea1c2d07e9eefcd2b33
David Rouyer <rouyer.david@gmail.com>
2022-10-11 23:29:15 +0200

    feat: introduce NoContentException

---
+++ src/Pipedrive.net/Exceptions/NoContentException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+namespace Pipedrive
+{
+    /// <summary>
+    /// Represents a HTTP 204 - No content response returned from the API.
+    /// </summary>
+    public class NoContentException : ApiException
+    {
+        /// <summary>
+        /// Constructs an instance of NoContentException
+        /// </summary>
+        /// <param name="response">The HTTP payload from the server</param>
+        public NoContentException(IResponse response) : this(response, null)
+        {
+        }
+
+        /// <summary>
+        /// Constructs an instance of NoContentException
+        /// </summary>
+        /// <param name="response">The HTTP payload from the server</param>
+        /// <param name="innerException">The inner exception</param>
+        public NoContentException(IResponse response, Exception innerException)
+            : base(response, innerException)
+        {
+            Debug.Assert(response != null && response.StatusCode == HttpStatusCode.NoContent,
+                "NoContentException created with wrong status code");
+        }
+
+        public override string Message
+        {
+            get { return ApiErrorMessageSafe ?? "No content"; }
+        }
+    }
+}
